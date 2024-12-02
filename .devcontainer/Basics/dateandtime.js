62d473577ad3852f6b2cdac949294ee2dd9aecb5
Sumit-Code0305 <sumittiwari63929@gmail.com>
2024-12-02 15:23:53 +0000

    Pushed my date and time file

---
+++ .devcontainer/Basics/dateandtime.js
@@ -0,0 +1,8 @@
+let myDate=new Date();
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
+let myTimeStamp=Date.now();
+console.log(myTimeStamp)
