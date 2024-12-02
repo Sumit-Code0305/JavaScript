1fd2a78379eab140cd584b849353232e9c44eae1
Sumit-Code0305 <sumittiwari63929@gmail.com>
2024-12-02 11:31:34 +0000

    pushed my datatype file

---
+++ .devcontainer/Basics/datatype.js
@@ -0,0 +1,9 @@
+"use strict";
+/*alert('hello')
+I am using NodeJs not Browser
+*/
+let name='sumit tiwari';//string type//can also written in single quote or double quote
+let age=24;//number type for large number use BigInt type
+let isLoggedIn=true;//boolean type
+
+console.table([typeof name,typeof age,typeof isLoggedIn,typeof null,typeof undefined])
