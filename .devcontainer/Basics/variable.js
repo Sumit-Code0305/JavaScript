dc4bab659c75ab466d1a9e903ab7c9ecf566e13f
Sumit-Code0305 <sumittiwari63929@gmail.com>
2024-12-02 11:12:04 +0000

    push my varibale file

---
+++ .devcontainer/Basics/variable.js
@@ -0,0 +1,9 @@
+const accountId=12;//can't change it in next time
+let accountEmail="sumittiwari";
+var accountPassword='12345';
+accountCity='Gogdfs';
+//accountId=2;
+accountEmail='gfdssefwedgw';
+accountPassword='fsgdd';
+accountCity='dhfss';
+console.table([accountId,accountEmail,accountPassword,accountCity]);
