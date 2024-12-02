40a2fde30f020a7de96de97427a531866a076479
Sumit-Code0305 <sumittiwari63929@gmail.com>
2024-12-02 11:44:45 +0000

    pushed my datatype-conversion file

---
+++ .devcontainer/Basics/datatype-conversion.js
@@ -0,0 +1,10 @@
+let score="33";
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber))
+/*
+"33" will convert in 33 as a number,
+"33abc",type will convert as a number nut value will be NaN(not a number)
+*/ 
