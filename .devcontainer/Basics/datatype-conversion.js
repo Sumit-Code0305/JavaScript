b2b176be517ed6b18e90ad70ccd6d326bd29198f
Sumit-Code0305 <sumittiwari63929@gmail.com>
2024-12-02 12:00:14 +0000

    add my datatype-conversion file

--- .devcontainer/Basics/datatype-conversion.js
+++ .devcontainer/Basics/datatype-conversion.js
@@ -1,10 +1,28 @@
-let score="33";
-console.log(typeof score)
-console.log(typeof(score));
+// let score="33";
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valueInNumber=Number(score);
-console.log(typeof(valueInNumber))
-/*
-"33" will convert in 33 as a number,
-"33abc",type will convert as a number nut value will be NaN(not a number)
-*/ 
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber))
+// /*
+// "33" will convert in 33 as a number,
+// "33abc",type will convert as a number nut value will be NaN(not a number)
+// */ 
+// let value=3;
+// console.log(value)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let str1='hello';
+let str2=' sumit';
+console.log(str1+str2);
+
+
+
+
+
+
+
